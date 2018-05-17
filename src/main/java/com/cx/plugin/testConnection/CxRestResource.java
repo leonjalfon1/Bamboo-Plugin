fbd2d400d21161eb186843fd1476ea6bedfbd95c
Gal Nussbaum <galnus2@gmail.com>
2018-05-17 18:43:20 +0300

    fix the login failed message

--- src/main/java/com/cx/plugin/testConnection/CxRestResource.java
+++ src/main/java/com/cx/plugin/testConnection/CxRestResource.java
@@ -5,6 +5,7 @@ import com.cx.plugin.dto.TestConnectionResponse;
 import com.cx.restclient.CxShragaClient;
 import com.cx.restclient.dto.Team;
 import com.cx.restclient.sast.dto.Preset;
+import freemarker.template.TemplateMethodModelEx;
 import org.codehaus.plexus.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -75,15 +76,19 @@ public class CxRestResource {
                 statusCode = 200;
 
             } else {
-                if (result.equals("")) {
-                    result = "Login failed";
-                }
+                result = result.startsWith("Login failed.")? result: "Login failed. " + result;
                 presets = new ArrayList<Preset>() {{
-                    add(new Preset(NO_PRESET_ID, NO_PRESET_MESSAGE));
+                    Preset preset = new Preset();
+                    preset.setId(NO_PRESET_ID);
+                    preset.setName(NO_PRESET_MESSAGE);
+                    add(preset);//TODO update the common client
                 }};
 
                 teams = new ArrayList<Team>() {{
-                    add(new Team(NO_TEAM_PATH, NO_TEAM_MESSAGE));
+                    Team team = new Team();
+                    team.setId(NO_TEAM_PATH);
+                    team.setFullName(NO_TEAM_MESSAGE);
+                    add(team);//TODO update the common client
                 }};
 
                 tcResponse = new TestConnectionResponse(result, presets, teams);
