96a08e39c7dd97f2a68b32de57e3fb6b68bac0b2
CxGalnus <gal.nussbaum@checkmarx.com>
2018-05-17 17:33:38 +0300

    REST MIGRATION- Integrate the new client

--- src/main/java/com/cx/plugin/results/CxJobResultsWebPanel.java
+++ src/main/java/com/cx/plugin/results/CxJobResultsWebPanel.java
@@ -2,11 +2,12 @@ package com.cx.plugin.results;
 
 import com.atlassian.bamboo.resultsummary.BuildResultsSummaryImpl;
 import com.atlassian.plugin.web.model.WebPanel;
-import com.cx.plugin.utils.CxResultUtils;
 
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Map;
+
+import static com.cx.plugin.utils.CxParam.HTML_REPORT;
 
 /**
  * Created by: dorg.
@@ -17,11 +18,10 @@ public class CxJobResultsWebPanel implements WebPanel {
 
 
     public String getHtml(Map<String, Object> map) {
-        CxResultUtils resultsUtils = new CxResultUtils();
         BuildResultsSummaryImpl a = (BuildResultsSummaryImpl) map.get("resultSummary");
-        Map<String, String> customBuildData = a.getCustomBuildData();
-        String ret = resultsUtils.resolveCostumeBuildData(customBuildData);
-        return ret;
+        Map<String, String> results = a.getCustomBuildData();
+
+        return results.get(HTML_REPORT);
     }
 
     public void writeHtml(Writer writer, Map<String, Object> map) throws IOException {
